69c36402adc7fe48e4b8c81a8242d1ca869b1cd6
davkimmy159 <davkimmy159@gmail.com>
2016-05-28 15:52:56 +0900

    AJAX

--- src/main/java/kr/co/ps119/controller/demo/DemoController.java
+++ src/main/java/kr/co/ps119/controller/demo/DemoController.java
@@ -23,6 +23,7 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Controller
@@ -32,32 +33,40 @@ public class DemoController {
 	@Autowired
 	DemoService demoService;
 
-	@RequestMapping(value = "ajaxTest1")
+	@RequestMapping(value = "ajaxTest1", method = RequestMethod.GET)
 	@ResponseBody
-	public Map<String, Object> ajaxTest1() {
-		
-		// result map object
+	public Map<String, Object> ajaxTest1(@RequestParam(value = "nickname") String nickname, HttpSession session) {
+		MemberUser memberUser = new MemberUser(nickname + "@mail.com", nickname);
+
+		Long id = demoService.save(memberUser).getId();
+
 		Map<String, Object> jsonObject = new HashMap<>();
-		
-		jsonObject.put("list", demoService.findAll());
-		jsonObject.put("size", new Integer(4));
-		
+
+		List<MemberUser> list = demoService.findAll();
+
+		jsonObject.put("list", list);
+		jsonObject.put("size", list.size());
+		jsonObject.put("id", id);
+
+		System.out.println(session);
 		return jsonObject;
 	}
-	
+
 	@RequestMapping(value = "ajaxTest2", method = RequestMethod.GET)
-	public void ajaxTest2(@RequestParam(value = "nickname") String nickname) {
-		MemberUser memberUser = new MemberUser("*", nickname);
-		demoService.save(memberUser);
-	}
-/*	
-	@RequestMapping(value = "ajaxTest2")
 	@ResponseBody
-	public List<MemberUser> ajaxTest2() {
+	public Map<String, Object> ajaxTest2(@RequestParam(value = "id") Long id) {
+		demoService.delete(id);
+
+		Map<String, Object> jsonObject = new HashMap<>();
+		
 		List<MemberUser> list = demoService.findAll();
-		return list;
+
+		jsonObject.put("list", list);
+		jsonObject.put("size", list.size());
+
+		return jsonObject;
 	}
-*/	
+
 	@RequestMapping(value = "test")
 	public String test() {
 		return "test";
@@ -79,41 +88,28 @@ public class DemoController {
 	}
 
 	/*
-	@RequestMapping(method = RequestMethod.GET)
-	public String printList(Model model) {
-		List<MemberUser> list = demoService.findAll();
-		model.addAttribute("list", list);
-		if (!model.containsAttribute("memberUserForm"))
-			model.addAttribute("memberUserForm", null);
-		return "member_table";
-	}
-	
-	@RequestMapping(value = "control", method = RequestMethod.POST, params = "insert")
-	public String insert(Model model, @Validated MemberUserForm memberUserForm, BindingResult result) {
-		if (result.hasErrors()) {
-			return printList(model);
-		}
-		MemberUser memberUser = new MemberUser();
-		BeanUtils.copyProperties(memberUserForm, memberUser);
-		MemberUser addedMemberUse = demoService.save(memberUser);
-		return printList(model);
-	}
-	
-	@RequestMapping(value = "control", method = RequestMethod.POST, params = "update")
-	public String update(Model model, @RequestParam(name = "modId") Long modId) {
-		demoService.delete(modId);
-		return printList(model);
-	}
-	
-	@RequestMapping(value = "control", method = RequestMethod.POST, params = "delete")
-	public String delete(Model model, @RequestParam(name = "modId") Long modId) {
-		demoService.delete(modId);
-		return printList(model);
-	}
-	
-	@RequestMapping(value = "writing", method = RequestMethod.POST)
-	public String writing() {
-		return "a";
-	}
-	*/
+	 * @RequestMapping(method = RequestMethod.GET) public String printList(Model
+	 * model) { List<MemberUser> list = demoService.findAll();
+	 * model.addAttribute("list", list); if
+	 * (!model.containsAttribute("memberUserForm"))
+	 * model.addAttribute("memberUserForm", null); return "member_table"; }
+	 * 
+	 * @RequestMapping(value = "control", method = RequestMethod.POST, params =
+	 * "insert") public String insert(Model model, @Validated MemberUserForm
+	 * memberUserForm, BindingResult result) { if (result.hasErrors()) { return
+	 * printList(model); } MemberUser memberUser = new MemberUser();
+	 * BeanUtils.copyProperties(memberUserForm, memberUser); MemberUser
+	 * addedMemberUse = demoService.save(memberUser); return printList(model); }
+	 * 
+	 * @RequestMapping(value = "control", method = RequestMethod.POST, params =
+	 * "update") public String update(Model model, @RequestParam(name = "modId")
+	 * Long modId) { demoService.delete(modId); return printList(model); }
+	 * 
+	 * @RequestMapping(value = "control", method = RequestMethod.POST, params =
+	 * "delete") public String delete(Model model, @RequestParam(name = "modId")
+	 * Long modId) { demoService.delete(modId); return printList(model); }
+	 * 
+	 * @RequestMapping(value = "writing", method = RequestMethod.POST) public
+	 * String writing() { return "a"; }
+	 */
 }
--- src/main/resources/testData/data.sql
+++ src/main/resources/testData/data.sql
@@ -1,4 +1,6 @@
+/*
 INSERT INTO member_user(email, username) VALUES('*', 'David');
 INSERT INTO member_user(email, username) VALUES('*', 'Michael');
 INSERT INTO member_user(email, username) VALUES('*', 'Kim');
 INSERT INTO member_user(email, username) VALUES('*', 'Dilara');
+*/
