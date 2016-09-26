6b5f6071cd266126c1bbf5ec1b33bc866a00e1a8
SeongTae Kim <test@gmail.com>
2016-09-26 18:32:30 +0900

    asdf

--- src/main/java/kr/co/ps119/ServletInitializer.java
+++ src/main/java/kr/co/ps119/ServletInitializer.java
@@ -1,7 +1,8 @@
 package kr.co.ps119;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+// import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 	@Override
